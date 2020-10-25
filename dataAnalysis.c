745f59d60dcfcc8f73620c5d54983658be10ee39
shreyas115 <shre_prakash@yahoo.com>
2020-10-25 17:27:51 +0530

    Latest working version

--- dataAnalysis.c
+++ dataAnalysis.c
@@ -25,16 +25,22 @@ void readPosFile(int fd)
   }
 
  int len= strlen(allPosLines);
- char *word = malloc(500);
+ char word[20];
  words=0;
+ int noOfChar=0;
  for(i=0;i<len;i++)
  {
 	 word[count++]=allPosLines[i];
 	 if(allPosLines[i]=='\n')
 	{
+		char *str = (char *)malloc(sizeof(char)*count);
 		words++;
-		posWordArray[words-1] = word;
+		for(noOfChar=0;noOfChar<count;noOfChar++)
+			*(str+noOfChar)=word[noOfChar];
+		*(str+count-1)='\0';
+		posWordArray[words-1]=malloc(count);
 		//write(0,posWordArray[words-1],count);
+		strcpy(posWordArray[words-1],str);
 		count = 0;
 		posWordCount++;
 	}
@@ -44,22 +50,29 @@ void readNegFile(int fd)
 {
   int i,n,count,words;
   count=0;
-  char* allLines = malloc(500);
+  char* allNegLines = malloc(500);
+  memset(buf,0,512);
   while((n = read(fd, buf, sizeof(buf))) > 0){
-      	strcpy(allLines,buf);	      
+      	strcpy(allNegLines,buf);	      
     }
 
- int len = strlen(allLines);
- char *word = malloc(500);
+ int len = strlen(allNegLines);
+ char word[20];
  words=0;
+ int noOfChar=0;
  for(i=0;i<len;i++)
  {
-	 word[count++]=allLines[i];
-	 if(allLines[i]=='\n')
-	{
+	 word[count++]=allNegLines[i];
+	 if(allNegLines[i]=='\n')
+	{	
+		char*str = (char *)malloc(sizeof(char)*count);
 		words++;
-		negWordArray[words-1] = word;
+		for(noOfChar=0;noOfChar<count;noOfChar++)
+			*(str+noOfChar)=word[noOfChar];
+		*(str+count-1)='\0';
+		negWordArray[words-1] = malloc(count);
 		//write(0,negWordArray[words-1],count);
+		strcpy(negWordArray[words-1],str);
 		count = 0;
 		negWordCount++;
 	}
@@ -76,23 +89,23 @@ void readReviewFile(int fd)
 
  int len = strlen(allLines);
  char word[20];
- char filteredWord[20];
  int noOfChars = 0;
  words=0;
  for(i=0;i<len;i++)
  {
 	 word[count++]=allLines[i];
-	 if(allLines[i]==' ')
+	 if(!((allLines[i] >= 'a' && allLines[i] <= 'z') || (allLines[i] >= 'A' && allLines[i] <= 'Z')))
 	{
+		char *str = (char *)malloc(sizeof(char)*count);
 		words++;
 		for(noOfChars = 0;noOfChars<count; noOfChars++)
-			filteredWord[noOfChars]=word[noOfChars];
-		reviewWordArray[words-1]=malloc(20);
-		strcpy(reviewWordArray[words-1], filteredWord);
+			*(str+noOfChars)=word[noOfChars];
+		*(str+count-1)='\0';
+		reviewWordArray[words-1]=malloc(count);
+		strcpy(reviewWordArray[words-1], str);
 		//printf(1,"word : %s  %d\n",reviewWordArray[words-1],count+5);
 		count = 0;
 		reviewCount++;
-		memset(filteredWord,' ',20);
 	}
  }
 }
@@ -125,25 +138,32 @@ if((fd = open(argv[2],0)) < 0){
   int j =0;
   int pos = 0;
   int neg = 0;
-  for (j=0;j<reviewCount;j++)
-	printf(1,"word _%s\n_",reviewWordArray[j]);
+ /* for (j=0;j<reviewCount;j++)
+	printf(1,"word :%s\n",reviewWordArray[j]);
   for(j=0;j<posWordCount;j++)
-	printf(1,"pos _%s\n_",posWordArray[j]);
-
-			
+	printf(1,"pos :%s\n",posWordArray[j]);
+*/
   for(j = 0;j<reviewCount;j++)
   {
 	  //printf(1,"word : %s \n",reviewWordArray[j]);
 	  for(pos=0;pos<posWordCount;pos++)
-	  {
+	  {	  
+		  //printf(1,"word is _%s_",posWordArray[pos]);
+		  //printf(1,"review is _%s_",reviewWordArray[j]);
 		  if(strcmp(reviewWordArray[j],posWordArray[pos])==0)
+		  {
 				  finalPosCount++;
+				  printf(1, "Pos Word _%s_\n",posWordArray[pos]);
+		  }
 
 	  }
 	  for(neg=0;neg<negWordCount;neg++)
 	  {
 	  	if(strcmp(reviewWordArray[j],negWordArray[neg])==0)
+		{
 				finalNegCount++;
+				printf(1,"Neg Word_%s_\n",negWordArray[neg]);
+		}		
 	  }
   }
   printf(1,"Pos word count %d \n",finalPosCount);
